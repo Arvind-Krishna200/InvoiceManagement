daa043001a416cbe08fd7b267ebf27eccd0c10b5
Acer <Acer@DESKTOP-BJGK7IS>
2025-06-27 13:38:03 +0530

    Checking the git commit push workings.

--- InvoiceManagement/Models/InvoiceHeader.cs
+++ InvoiceManagement/Models/InvoiceHeader.cs
@@ -4,19 +4,14 @@ using System.ComponentModel.DataAnnotations;
 public class InvoiceHeader
 {
     public int InvoiceID { get; set; }
-
    
     public string InvoiceNumber { get; set; }
-
     
     public string Title { get; set; }
 
-
     public DateTime InvoiceDate { get; set; }
 
-
     public decimal TotalAmount { get; set; } 
-
 
     public List<InvoiceLineItem> LineItems { get; set; } = new();
 
